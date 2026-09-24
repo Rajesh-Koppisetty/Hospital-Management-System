cc1eb551c5aa727a9eb842eeee947324c44c62e6
Rajesh98676 <rajeshkoppisetty12@gmail.com>
2026-09-24 16:53:58 +0530

    deleted

--- hospital-management-frontend/eslint.config.js
+++ hospital-management-frontend/eslint.config.js
@@ -8,9 +8,7 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    //hello
     extends: [
-      //hello
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
